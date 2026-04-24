c66a9e79b5b7d581a28d331624c31faa23b8ab46
Divyanshu Raj <divyanshuraj23102005@gmail.com>
2026-04-24 17:54:54 +0530

    Basic server created

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/", (req, res) =>{
+    res.send("Hello from the server!!");
+    
+});
+app.use("/test",(req, res)=>{
+    res.send("testing !");
+});
+
+app.listen(7777, ()=>{
+    console.log("Listening to port 7777...");
+});
