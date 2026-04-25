ea4ba85ad9f3cbbe7ee656bd68a1fe516974d048
Divyanshu Raj <divyanshuraj23102005@gmail.com>
2026-04-25 15:25:15 +0530

    exploring https methods

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@ const express = require('express');
 
 const app = express();
 
+app.post("/hello", (req, res) =>{
+    res.send("post here");
+});
+
+app.delete("/hello", (req, res) =>{
+    res.send("delete");
+});
+app.patch("/hello", (req, res)=>{
+    res.send("patch");
+});
+app.put("/hello", (req, res)=>{
+    res.send("put");
+});
+app.get("/hello", (req,res)=> {
+    res.send("get here");
+});
+app.use("/hello", (req, res)=>{
+    res.send("from hello route!!")
+});
 app.use("/", (req, res) =>{
     res.send("Hello from the server!!");
     
