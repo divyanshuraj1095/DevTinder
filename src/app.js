1bb15416a2d46c5f44af3ffead67b0dda84c34a2
Divyanshu Raj <divyanshuraj23102005@gmail.com>
2026-04-26 15:30:34 +0530

    Written both try-catch error handler and "/" error handling ways

--- src/app.js
+++ src/app.js
@@ -4,28 +4,50 @@ const {authAdmin, authUser} = require("./middlewares/auth.js");
 
 const app = express();
 
-app.use("/admin", authAdmin);
+// app.use("/admin", authAdmin);
 
-app.get("/admin/getAllData", (req, res, next)=>{
-       res.send("All data send !!");
-});
+// app.get("/admin/getAllData", (req, res, next)=>{
+//        res.send("All data send !!");
+// });
 
-app.use("/user", authUser,(req, res, next)=>{
-    console.log("response 1");
-    next();
-},
-(req,res, next)=>{
-    // res.send("handler1");
-    console.log("response 2");
-    next();
-    // res.send("handler 2nd call");
-}, 
-(req, res, next)=>{
-    res.send("handler 3 rd call");
-}
+// app.get("/user/login", (req, res, next)=>{
+//        res.send("Login Successful");
+// });
 
 
-);
+
+// app.use("/user", authUser,(req, res, next)=>{
+//     console.log("response 1");
+//     next();
+// },
+// (req,res, next)=>{
+//     // res.send("handler1");
+//     console.log("response 2");
+//     next();
+//     // res.send("handler 2nd call");
+// }, 
+// (req, res, next)=>{
+//     res.send("handler 3 rd call");
+// }
+// );
+
+app.use("/user", (req, res)=>{
+    try{
+        throw new err;
+        res.send("User Details!!");
+    }
+    catch (err){
+        res.status(401).send("Something is wrong");
+    }
+    
+    
+})
+
+app.use("/", (err, req, res, next)=>{
+    if(err){
+        res.status(401).send("Something went wrong");
+    }
+})
 
 
 
